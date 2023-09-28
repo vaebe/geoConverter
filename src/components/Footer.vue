19af1d435282c4e052dee93467936463d2a624c5
zcj <18137693952@163.com>
2023-09-28 15:20:22 +0800

    feat: 增加 unocss 集成

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,13 @@
 </script>
 
 <template>
-  <div style="height: 100px">
-    本站总访问量<span id="busuanzi_value_site_pv" />次
-    本站访客数<span id="busuanzi_value_site_uv" />人次
+  <div class="flex justify-center">
+    <p class="mr">
+      本站总访问量<span id="busuanzi_value_site_pv" />次
+    </p>
+    <p>
+      本站访客数<span id="busuanzi_value_site_uv" />人次
+    </p>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
