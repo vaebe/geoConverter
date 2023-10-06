2ae07a249d3232e74512cf276e8d9b3bf2c471e2
zcj <18137693952@163.com>
2023-10-06 14:49:09 +0800

    feat: 移除为使用的组件

--- src/views/geoCoordinateConversion/index.vue
+++ src/views/geoCoordinateConversion/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
 import { coordinateSystemsObj, useGeoCoordinateConversion } from './useGeoCoordinateConversion.ts'
-
-const CoordinateSystemsInfo = defineAsyncComponent(() => import('./components/CoordinateSystemsInfo.vue'))
 
 const { oldCodeText, oldCodeType, newCodeText, newCodeType, transformCoordinateSystem, initClipboard } = useGeoCoordinateConversion()
 initClipboard()
