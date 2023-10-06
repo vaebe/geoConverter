b8ee20b75d840be9b094c10358ff6bc55ff2bb1e
zcj <18137693952@163.com>
2023-10-06 15:32:38 +0800

    style(header): 组件样式调整

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,14 @@
 </script>
 
 <template>
-  <div class="m-2 text-center font-normal">
+  <div class="m-2 text-center font-normal sticky top-0 z-10 bg-white">
     <h1>geo 地理坐标转换</h1>
-    <h3 class="mt2 text-gray-500">
+
+    <h3 class="text-gray-600">
       支持 js 一维、二维数组、geoJson 数据格式
     </h3>
+
+    <el-divider></el-divider>
   </div>
 </template>
 
